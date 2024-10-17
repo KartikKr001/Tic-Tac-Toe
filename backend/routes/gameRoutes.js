a2243c2e1edad19098aa474937acc246fe0c62c0
Kartik Kumar <kartikkrprajapati01@gmail.com>
2024-10-17 13:20:42 +0530

    Made some changes

--- backend/routes/gameRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Example route (you can add more as needed)
-router.get('/status', (req, res) => {
-  res.send('Game server is running');
-});
-
-module.exports = router;
